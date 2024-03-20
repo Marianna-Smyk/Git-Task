be51ddc96784b6f6d9dbc1da06d184174fd666e9
Marianna-Smyk <157255930+Marianna-Smyk@users.noreply.github.com>
2024-03-20 13:17:00 +0000

    Add files via upload

---
+++ add_function.py
@@ -0,0 +1,14 @@
+def add_2(var_num):
+    var_num += 2
+
+    return var_num
+
+test_1 = add_2(-5)
+test_2 = add_2(13)
+test_3 = add_2(70)
+test_4 = add_2(99)
+
+print(test_1)
+print(test_2)
+print(test_3)
+print(test_4)
